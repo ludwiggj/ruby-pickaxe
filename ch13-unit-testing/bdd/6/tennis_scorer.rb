9e72f78f532e88a60919d5d89f7ce62ad53f484f
Graeme Ludwig <graeme.ludwig@btopenworld.com>
2017-02-23 20:19:42 +0000

    Finished tennis exercise

--- ch13-unit-testing/bdd/6/tennis_scorer.rb
+++ ch13-unit-testing/bdd/6/tennis_scorer.rb
@@ -4,15 +4,51 @@ class TennisScorer
 
   SCORES = [0, 15, 30, 40]
   
-  @status = :in_progress
-
   def initialize
     @score = { :server => 0, :receiver => 0 }
+    @game_over = false
+  end
+ 
+  def both_players_have_won_at_least_three_points
+    @score[:server] >= 3 && @score[:receiver] >= 3
+  end
+
+  def player_has_won(player)
+    if (both_players_have_won_at_least_three_points)
+      other = OPPOSITE_SIDE_OF_NET[player]
+      @score[player] - @score[other] == 2
+    else
+      @score[player] == 4
+    end
+  end
+
+  def won(player)
+    case player
+    when :server
+      "W-L"
+    when :receiver
+      "L-W"
+    end
+  end
+
+  def advantage(player)
+    "A-#{player.to_s}"
   end
   
   def score
-    if (@status == :deuce)
-      "Deuce"
+    if player_has_won(:server)
+      won(:server)
+    elsif player_has_won(:receiver)
+      won(:receiver)
+    elsif both_players_have_won_at_least_three_points
+      case (@score[:server] - @score[:receiver])
+      when 1
+        advantage(:server)
+      when 0
+        "Deuce"
+      when -1
+        advantage(:receiver)
+      end
     else
       "#{SCORES[@score[:server]]}-#{SCORES[@score[:receiver]]}"
     end
@@ -20,11 +56,12 @@ class TennisScorer
   
   def give_point_to(player)
     other = OPPOSITE_SIDE_OF_NET[player]
-    fail "Unknown player #{player}" unless other
-    @score[player] += 1
 
-    if (@score[player] == 3) && (@score[other] == 3)
-      @status = :deuce
-    end
+    fail "Unknown player #{player}" unless other
+
+    fail "Game over, result: #{score}" if @game_over
+
+    @score[player] += 1
+    @game_over = player_has_won(player)
   end
 end
--- ch13-unit-testing/bdd/6/ts_spec.rb
+++ ch13-unit-testing/bdd/6/ts_spec.rb
@@ -3,7 +3,7 @@ require "test/unit"
 
 describe "TennisScorer", "basic scoring" do
   let(:ts) { TennisScorer.new }
-  
+
   it "should start with a score of 0-0" do
     expect(ts.score).to eq("0-0")
   end
@@ -34,19 +34,56 @@ describe "TennisScorer", "basic scoring" do
     expect(ts.score).to eq("40-0")
   end
   
-  it "should raise an exception if award point to the umpire" do
-    expect{ ts.give_point_to(:umpire) }.to raise_error(RuntimeError, /Unknown player umpire/)
-  end
-
   it "should be Deuce after each wins three points" do
     3.times do ts.give_point_to(:server) end
     3.times do ts.give_point_to(:receiver) end
     expect(ts.score).to eq("Deuce")
   end
   
-  it "should be W-L after the server wins four points"
+  it "should be A-server after each wins three points and the server gets one more" do
+    3.times do ts.give_point_to(:receiver) end
+    4.times do ts.give_point_to(:server) end
+    expect(ts.score).to eq("A-server")
+  end
   
-  it "should be L-W after the receiver wins four points"
+  it "should be A-receiver after each wins three points and the receiver gets one more" do
+    3.times do ts.give_point_to(:server) end
+    4.times do ts.give_point_to(:receiver) end
+    expect(ts.score).to eq("A-receiver")
+  end
   
-  it "should be A-server after each wins three points and the server gets one more"
+  it "should be W-L after the server wins four points" do
+    4.times do ts.give_point_to(:server) end
+    expect(ts.score).to eq("W-L")
+  end
+  
+  it "should be L-W after the receiver wins four points" do
+    4.times do ts.give_point_to(:receiver) end
+    expect(ts.score).to eq("L-W")
+  end
+  
+  it "should be W-L after deuce and the server wins by two clear points" do
+    3.times do ts.give_point_to(:server) end
+    3.times do ts.give_point_to(:receiver) end
+    ts.give_point_to(:receiver)
+    3.times do ts.give_point_to(:server) end
+    expect(ts.score).to eq("W-L")
+  end
+  
+  it "should be L-W after deuce and the receiver wins by two clear points" do
+    3.times do ts.give_point_to(:server) end
+    3.times do ts.give_point_to(:receiver) end
+    ts.give_point_to(:receiver)
+    2.times do ts.give_point_to(:server) end
+    3.times do ts.give_point_to(:receiver) end
+    expect(ts.score).to eq("L-W")
+  end
+  
+  it "should raise an exception if award point to the umpire" do
+    expect{ ts.give_point_to(:umpire) }.to raise_error(RuntimeError, /Unknown player umpire/)
+  end
+
+  it "should raise an exception if award point when the game is complete" do
+    expect{ 5.times do ts.give_point_to(:server) end }.to raise_error(RuntimeError, /Game over, result: W-L/)
+  end
 end
